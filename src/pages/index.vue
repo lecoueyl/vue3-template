3bb65b154b4d84528d3095e6818d596f22e33bc6
Léonard Lecouey <leonard.lecouey@gmail.com>
2022-01-18 10:58:20 +0900

    style(index): improve transition and alignment

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,53 +8,55 @@ onMounted(() => {
 </script>
 
 <template>
-  <header class="container flex justify-end p-4 mx-auto">
-    <button
-      class="overflow-hidden p-2"
-      @click="switchTheme()"
-    >
-      <transition
-        enter-active-class="transition duration-200 ease-out"
-        leave-active-class="transition duration-200 ease-in"
-        enter-from-class="transform -translate-y-full scale-50"
-        enter-to-class="transform translate-y-0"
-        leave-from-class="transform translate-y-0"
-        leave-to-class="transform translate-y-full scale-50"
-        mode="out-in"
+  <header class="absolute inset-x-0 top-0">
+    <div class="container flex justify-end p-4 mx-auto">
+      <button
+        class="overflow-hidden p-2"
+        @click="switchTheme()"
       >
-        <svg
-          v-if="currentTheme === 'dark'"
-          xmlns="http://www.w3.org/2000/svg"
-          class="w-8 h-8"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
+        <transition
+          enter-active-class="transition duration-200 ease-out"
+          leave-active-class="transition duration-200 ease-in"
+          :enter-from-class="currentTheme === 'dark' ? 'transform -translate-y-full scale-50 opacity-0' : 'transform translate-y-full scale-50 opacity-0'"
+          enter-to-class="transform translate-y-0"
+          leave-from-class="transform translate-y-0"
+          :leave-to-class="currentTheme === 'dark' ? 'transform translate-y-full scale-50 opacity-0' : 'transform -translate-y-full scale-50 opacity-0'"
+          mode="out-in"
         >
-          <path d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
-        </svg>
+          <svg
+            v-if="currentTheme === 'dark'"
+            xmlns="http://www.w3.org/2000/svg"
+            class="w-8 h-8"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+          >
+            <path d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+          </svg>
 
-        <svg
-          v-else
-          xmlns="http://www.w3.org/2000/svg"
-          class="w-8 h-8"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-        >
-          <path d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
-        </svg>
-      </transition>
-    </button>
+          <svg
+            v-else
+            xmlns="http://www.w3.org/2000/svg"
+            class="w-8 h-8"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+          >
+            <path d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+          </svg>
+        </transition>
+      </button>
+    </div>
   </header>
 
-  <main class="px-4 mx-auto max-w-3xl">
-    <h1 class="py-24 text-6xl text-center text-gray-700 dark:text-gray-100 transition-colors">
+  <main class="flex flex-col justify-center items-center py-16 px-4 mx-auto max-w-3xl h-screen">
+    <h1 class="pb-20 text-6xl text-center text-gray-700 dark:text-gray-100 transition-colors">
       Vue.js starter template
     </h1>
 
