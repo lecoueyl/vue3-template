06ed32bb81547b59d64a78e1f51785432d59f89b
Léonard Lecouey <leonard.lecouey@gmail.com>
2022-03-16 21:53:16 +0900

    chore: update packages to last version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
   darkMode: 'class',
   theme: {
     extend: {
