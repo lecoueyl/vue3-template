acbf4bf5479f3323cafd5c5897cd88d20f926cbe
tanghongxin <hongxin.tang@hotmail.com>
2022-05-20 20:46:19 +0800

    ⚡️Antd 启用按需加载

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
 import '@/assets/styles/fonts.css';
 import '@/assets/styles/main.css';
 import App from '@/app.vue';
@@ -14,5 +12,4 @@ const router = createRouter({
 
 const app = createApp(App);
 app.use(router);
-app.use(Antd);
 app.mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import vue from '@vitejs/plugin-vue';
 import eslint from 'vite-plugin-eslint';
 import svgLoader from 'vite-svg-loader';
 import { resolve } from 'path';
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
@@ -13,6 +15,9 @@ export default ({ mode }) => {
       eslint({ cache: false }),
       svgLoader(),
       vue(),
+      Components({
+        resolvers: [AntDesignVueResolver()],
+      }),
     ],
     resolve: {
       alias: {
